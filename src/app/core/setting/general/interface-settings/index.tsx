295f72d2e705749360dbab8d828b4c2da1599d59
codexu <461229187@qq.com>
2026-02-03 17:53:19 +0800

    style: 调整默认模型配置位置

--- src/app/core/setting/general/interface-settings/index.tsx
+++ src/app/core/setting/general/interface-settings/index.tsx
@@ -1,6 +1,4 @@
 'use client'
-
-import { useTranslations } from 'next-intl'
 import { ThemeSettings } from './theme'
 import { LanguageSettings } from './language'
 import { ScaleSettings } from './scale'
@@ -11,11 +9,9 @@ import { CustomThemeSettings } from './custom-theme'
 import { TraySettings } from './tray-settings'
 
 export function InterfaceSettings() {
-  const t = useTranslations('settings.general.interface')
 
   return (
     <div className="space-y-4">
-      <h3 className="text-lg font-semibold mb-4">{t('title')}</h3>
       <ThemeSettings />
       <LanguageSettings />
       <ScaleSettings />
